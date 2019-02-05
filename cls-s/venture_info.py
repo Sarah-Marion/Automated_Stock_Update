28ae33713ae7da7c5a676250739b51c4e1315184
Sarah-Marion <devsarahmarion@gmail.com>
2019-02-05 18:29:46 +0300

    fetched latest article summary from endpoint to JSON

---
+++ cls-s/venture_info.py
@@ -0,0 +1,39 @@
+import requests
+
+class Info:
+    """
+    Class that makes a GET request to the IEX API
+    """
+    def __init__(self):
+        return
+
+    def get_summary(self, symbol, stock_url):
+        """
+        Returns the summary of the latest article that mentions that particular stock from IEX API endpoint
+        """
+        try:
+            # Fetch latest summary of latest article
+            response = requests.get(stock_url + symbol + '/news/last/1')
+            json = response.json()
+            return json [0]['summary']
+        
+        except Exception as e:
+            
+            print(str(e))
+
+
+    def get_articles(self, symbol, stock_url):
+        """
+        Returns the link to the latest article that mentions that particular stock from IEX API endpoint
+        """
+
+    def get_venture_name(self, symbol, stock_url):
+        """
+        Returns the venture name based on that stock symbol from IEX API endpoint
+        """
+
+    def get_exchange(self, symbol, stock_url):
+        """
+        Returns the stock exchange that stock belongs to from IEX API endpoint
+        """
+    
