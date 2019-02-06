531215954fd1e3c91d6d249fe206e381bc468746
Sarah-Marion <devsarahmarion@gmail.com>
2019-02-06 18:29:51 +0300

    returned nothing from taking one argument (body[body of email built from the builder class in temp_builder.py that will use the email.html

---
+++ cls-s/gmail.py
@@ -0,0 +1,13 @@
+import datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+
+# Method that establishes a connect to the Gmail server & sending an email to the designated receiver on my behalf
+
+class Gmail:
+    def __init__(self):
+        return
+
+
+    def send_email(self,body):
+        
