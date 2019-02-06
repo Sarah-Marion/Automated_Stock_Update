f43babb7ffbc67df886689ea81a90849f5214c70
Sarah-Marion <devsarahmarion@gmail.com>
2019-02-06 18:42:41 +0300

    Initial set up of Gmail Credentials - to input data slightly later

--- cls-s/gmail.py
+++ cls-s/gmail.py
@@ -10,4 +10,17 @@ class Gmail:
 
 
     def send_email(self,body):
+        try:
+            # Authentication for Gmail acc.
+            gmail_user = '' # set gmail address
+            gmail_password = '' # set gmail app pass
+
+
+        # Email Config set-up!
+        msg = MIMEMultipart('alternative')
+        msg.attach(body)
+        msg['From'] = '' # set from address
+        msg['To'] = '' # set to address
         
+        now = datetime.datetime.now()
+        msg['Subject'] = str(now.day) + '-' + str(now.month) + '-' + str(now.year) + 'Stock Insights'
