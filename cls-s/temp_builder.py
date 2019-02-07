c8f249bdc5f1381654977f476bdbcecda6dee210
Sarah-Marion <devsarahmarion@gmail.com>
2019-02-07 17:44:03 +0300

    Method that builds the email.html file to send via email with daily stock updates using jinja2 lib

--- cls-s/temp_builder.py
+++ cls-s/temp_builder.py
@@ -8,3 +8,18 @@ class Builder:
         return
 
     def build_template(self, symbols, summary, articles, venture_name, exchange):
+        #Using jinja2 to build email body
+        try:
+            file_loader = FileSystemLoader('temps')
+            env = Environment(loader = file_loader)
+            temp = env.get_template('email.html')
+            
+            # Building jinja2 temp from /temps/email.html
+            output = temp.sender(symbols = symbols, summary = summary, articles = articles, venture_name = venture_name, exchange = exchange)
+
+            body = MIMEText(output, 'html')
+            return body
+
+        except Exception as e:
+
+            print(str(e))
