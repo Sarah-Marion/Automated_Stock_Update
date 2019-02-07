b1bbb156a8ab740accfcb2cc8079d02cf94256ee
Sarah-Marion <devsarahmarion@gmail.com>
2019-02-07 17:35:47 +0300

    Established the connection  to the gmail server to send the email

---
+++ cls-s/temp_builder.py
@@ -0,0 +1,10 @@
+from email.mime.text import MIMEText
+from jinja2 import Environment, FileSystemLoader
+
+# Creating the connection to the Gmail server and send the email
+
+class Builder:
+    def __init__(self):
+        return
+
+    def build_template(self, symbols, summary, articles, venture_name, exchange):
